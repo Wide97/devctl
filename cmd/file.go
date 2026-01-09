fa9daf6439ff919a860c3a9a5f8f3269bacfa960
Arianna Benoni <arianna.benoni@anitia.it>
2026-01-09 14:42:32 +0100

    add file exists.go to fileutil

--- cmd/file.go
+++ cmd/file.go
@@ -1,20 +1,45 @@
 package cmd
 
-import "os"
+import (
+	"context"
+	file "devctl/internal/fileutil"
+	"fmt"
+	"os"
+)
 
 func runFile(args []string) {
 	if len(args) < 1 {
-		//output.Error("missing subcommand")
+		fmt.Println("Error! Missing option.")
 		os.Exit(1)
 	}
 
 	switch args[0] {
 	case "exists":
-		//runFileExists(args[1:])
-	case "ls":
-		//runFileList(args[1:])
+		runFileExists(args[1:])
 	default:
-		//output.Error("unknown subcommand: " + args[0])
+		fmt.Println("Unknown option:", args[0])
 		os.Exit(1)
+	}
+}
+
+func runFileExists(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Error! Missing path.")
+		os.Exit(1)
+	}
+
+	path := args[0]
+	baseURL := "http://localhost:8080"
+
+	exists, err := file.Exists(context.Background(), baseURL, path)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	if exists {
+		fmt.Println("file exists")
+	} else {
+		fmt.Println("file does not exist")
 	}
 }
