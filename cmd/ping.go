95245b85f063b7bb1d8aaef94ebb1966c57add34
Aaron Angel Salas Sanchez <aaron.salas@anitia.it>
2026-01-09 12:59:41 +0100

    updating to main again

--- cmd/ping.go
+++ cmd/ping.go
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 package cmd
 
 func runPing(args []string) {
-    // funzione di ping per confermare che servizio è raggiungibile (gestita da Aaron)
+	// funzione di ping per confermare che servizio è raggiungibile (gestita da Aaron)
 
 }
-
-
-=======
-package cmd
->>>>>>> origin/main
