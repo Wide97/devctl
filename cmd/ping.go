e5bff6489614714085a6cc93d26a978d14be872d
Aaron Angel Salas Sanchez <aaron.salas@anitia.it>
2026-01-12 09:40:06 +0100

    created ping and test ping w output pkg

--- cmd/ping.go
+++ cmd/ping.go
@@ -1,6 +1,21 @@
 package cmd
 
-func runPing(args []string) {
-	// funzione di ping per confermare che servizio è raggiungibile (gestita da Aaron)
+import (
+	"context"
+	"devctl/internal/ping"
+	"devctl/pkg/output"
+	"os"
+)
 
+func runPing(args []string) {
+	// Base URL di esempio; in produzione si potrebbe leggere da flag o config
+	baseURL := "http://localhost:8080"
+
+	err := ping.Check(context.Background(), baseURL)
+	if err != nil {
+		output.Error(err.Error())
+		os.Exit(1)
+	}
+
+	output.OK("service reachable")
 }
