9d1677871d7a9720dc7ab58e14052a26ec1305d0
MrZhouDawei <daweizhou2002@gmail.com>
2026-01-12 09:32:21 +0100

    FEAT: add cmd arg parser

--- cmd/calc.go
+++ cmd/calc.go
@@ -1,5 +1,26 @@
-// here add addition feature
-package main
+package cmd
 
-func add(x, y int) int {
+import (
+	"devctl/internal/calc"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func Calc() error {
+	if len(os.Args) < 5 {
+		return errors.New("usage: devctl calc op val1 val2")
+	}
+
+	if os.Args[1] != "calc" {
+		return errors.New("unknown command")
+	}
+
+	res, err := calc.Calculate(os.Args[2], os.Args[3], os.Args[4])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("The result is:%d\n", res)
+	return nil
 }
